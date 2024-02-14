ef517c583782dfc427efc7d8f0d30030b7c415f3
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 20:43:02 +0800

    added pseudo code

--- vowel_counter_for_alf.py
+++ vowel_counter_for_alf.py
@@ -1,7 +1,12 @@
 # ========== PSEUDO CODE ==========
+# - Printing "VOWEL COUNTER FOR ALF"
 # - Prompt the user to enter a word.
+
 # - Count variable
+
+# - Using def function
 # - Using 'for loop' function
+
 # - Displaying the result or counted vowels from the user's inputted character or string
 
 
