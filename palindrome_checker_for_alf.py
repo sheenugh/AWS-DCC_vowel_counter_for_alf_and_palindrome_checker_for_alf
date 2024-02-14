e44bda20151124fdd6e7f3cd455e82f67cb25d29
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 20:55:45 +0800

    added pseudo code: printing palindrome checker for alf

--- palindrome_checker_for_alf.py
+++ palindrome_checker_for_alf.py
@@ -1,4 +1,5 @@
 # ========== PSEUDO CODE ==========
+# - Printing the 'PALINDROME CHECKER FOR ALF' 
 # - Prompt the user to enter a word.
 # - Check if the entered word is a palindrome using def function
 # - Displaying the result if palindrome or not
