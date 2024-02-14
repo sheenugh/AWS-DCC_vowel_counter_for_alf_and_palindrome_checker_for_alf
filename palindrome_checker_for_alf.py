795c9d2f53b2047c4dadaf47315810c68654519a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 20:54:48 +0800

    pseudo code

---
+++ palindrome_checker_for_alf.py
@@ -0,0 +1,4 @@
+# ========== PSEUDO CODE ==========
+# - Prompt the user to enter a word.
+# - Check if the entered word is a palindrome using def function
+# - Displaying the result if palindrome or not
