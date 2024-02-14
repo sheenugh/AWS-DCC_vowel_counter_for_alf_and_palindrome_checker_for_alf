0b4fb67a15757c329c3afd7f2dc27192a861dc36
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 20:50:01 +0800

    done

--- vowel_counter_for_alf.py
+++ vowel_counter_for_alf.py
@@ -1,12 +1,24 @@
 # ========== PSEUDO CODE ==========
 # - Printing "VOWEL COUNTER FOR ALF"
-# - Prompt the user to enter a word.
+print("VOWEL COUNTER FOR ALF")
 
-# - Count variable
+# - Prompt the user to enter a word.
+characters = input("Enter a word/s:")
+
+
 
 # - Using def function
-# - Using 'for loop' function
+def count_vowels(text):
+    count = 0 # - Count variable
+    
+    for i in text: # - Using 'for loop' function
+        if (i in "aAeEiIoOuU"):
+            count += 1
+    return count
+
+result = count_vowels(characters)
 
 # - Displaying the result or counted vowels from the user's inputted character or string
+print("The number of vowels in", characters, "is", result)
 
 
