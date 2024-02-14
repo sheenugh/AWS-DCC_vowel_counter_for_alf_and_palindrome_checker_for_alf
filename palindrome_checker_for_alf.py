3be24f8e55c974d128bd6e60ea823158f704db07
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 21:02:50 +0800

    done

--- palindrome_checker_for_alf.py
+++ palindrome_checker_for_alf.py
@@ -1,5 +1,18 @@
 # ========== PSEUDO CODE ==========
 # - Printing the 'PALINDROME CHECKER FOR ALF' 
+print("PALINDROME CHECKER FOR ALF")
+
 # - Prompt the user to enter a word.
+characters = input("Input something:")
+
 # - Check if the entered word is a palindrome using def function
+def if_palindrome_or_not(text):
+    reverse_char = text[::-1]
+    if reverse_char == text:
+        print(text, "is a palindrome")
+    else:
+        print(text, "is not a palindrome")
+
 # - Displaying the result if palindrome or not
+if_palindrome_or_not(characters)
+
